63792596a1b23843f95cef627932377db304cdd1
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-31 15:00:57 -0700

    adding middleware

--- app.js
+++ app.js
@@ -5,11 +5,23 @@ const express = require('express');
 const app = express();
 
 // middleware
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // routes
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
-});
+  // set the url of the gif
+  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+  // render the hello-gif view, passing the gifUrl into the view to be displayed
+  res.render('hello-gif', { gifUrl })
+})
+
+app.get('/greetings/:name', (req, res) => {
+    const name = req.params.name;
+    res.render('greetings', { name });
+})
 
 
 // start server
