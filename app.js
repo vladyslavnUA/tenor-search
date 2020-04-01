d7546ecd30d6b2dfed4bbe473b021e4ceba6e2bf
vladyslavnUA <vladyslavn@yahoo.com>
2020-03-31 18:23:01 -0700

    added search form

--- app.js
+++ app.js
@@ -12,17 +12,14 @@ app.set('view engine', 'handlebars');
 
 // routes
 app.get('/', (req, res) => {
-  // set the url of the gif
-  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-  // render the hello-gif view, passing the gifUrl into the view to be displayed
-  res.render('hello-gif', { gifUrl })
+    console.log(req.query) // => "{ term: het"
+    res.render('home')
 })
 
 app.get('/greetings/:name', (req, res) => {
     const name = req.params.name;
     res.render('greetings', { name });
 })
-
 
 // start server
 
